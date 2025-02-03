b8f2e9f0475902dcba90f42454bf44e1685bd47c
Vinicius <viniciuscraveiroo@gmail.com>
2025-02-02 22:39:05 -0300

    FEAT: adicionado icon no public, criando rotas

--- apps/front-ng/src/app/app.routes.ts
+++ apps/front-ng/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule)},
+  {path: 'stock', loadChildren: () => import('./stock/stock.module').then(m => m.StockModule)},
+  {path: 'cripto', loadChildren: () => import('./cripto/cripto.module').then(m => m.CriptoModule)},
+  {path: 'card', loadChildren: () => import('./card/card.module').then(m => m.CardModule)},
+];
